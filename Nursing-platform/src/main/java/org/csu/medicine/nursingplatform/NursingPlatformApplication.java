94be97bde5b866f8f7318c471f53f3ad810ba78b
Xp0503 <275760490@qq.com>
2025-06-26 14:36:42 +0800

    test

---
+++ Nursing-platform/src/main/java/org/csu/medicine/nursingplatform/NursingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.csu.medicine.nursingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NursingPlatformApplication.class, args);
+    }
+
+}
---
+++ Nursing-platform/src/test/java/org/csu/medicine/nursingplatform/NursingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.medicine.nursingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
